f4cdefdc21955e7ae3b1fd7f336ecdaed5c54500
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-07-10 12:59:43 +0500

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by A'zamjon Abdumuxtorov on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
